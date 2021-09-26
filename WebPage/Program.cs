baade9678a89ee17be6c1e75e6ad95cbd480ec4c
Xavi Belloso <xbelloso@voxelgroup.net>
2021-09-26 12:21:57 +0200

    chore: add logging provider Serilog

--- WebPage/Program.cs
+++ WebPage/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace WebPage
 {
@@ -21,6 +22,11 @@ namespace WebPage
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.AddSerilog();
                 });
     }
 }
