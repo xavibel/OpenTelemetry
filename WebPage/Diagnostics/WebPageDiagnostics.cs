cd9202ac4843ffd494da00dbe0fccd8bac2a255a
Xavi Belloso <xbelloso@voxelgroup.net>
2021-09-26 12:03:46 +0200

    feat: add Diagnostics class

---
+++ WebPage/Diagnostics/WebPageDiagnostics.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace WebPage.Diagnostics
+{
+    
+    public class WebPageDiagnostics
+    {
+        private readonly ILogger _logger;
+
+        public WebPageDiagnostics(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger("WebPage");
+        }
+
+        public void HomeIndex()
+        {
+            LoggerMessage.Define<string>(LogLevel.Warning, WebPageClientIds.HomeIndexEventId,
+                "We are on Home Index with data {data}");
+        }
+    }
+
+    public class WebPageClientIds
+    {
+        public static EventId HomeIndexEventId = new EventId(100, nameof(HomeIndexEventId));
+    }
+}
