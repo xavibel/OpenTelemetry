717745781e01caf903af0677afb92172393e1af2
czhou <czhou@voxelgroup.net>
2021-10-04 14:08:11 +0200

    chore: add console project

---
+++ Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace Console
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var client = new ServiceBusClient(configuration["ServiceBus:ConnectionString"]);
+
+            var receiver = client.CreateReceiver("SC2021");
+            var message = await receiver.ReceiveMessageAsync();
+            
+            System.Console.WriteLine(message.Body.ToString());
+        }
+    }
+}
