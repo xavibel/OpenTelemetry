3b40118e3c51e95e0d3769cbb133ad9b8a50042f
czhoulin <carolina.z.lin@gmail.com>
2021-10-11 11:05:57 +0200

    refact: extract class

---
+++ WebPage/Diagnostics/WebPageClientIds.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace WebPage.Diagnostics
+{
+    public class WebPageClientIds
+    {
+        public static EventId HomeIndexEventId = new EventId(100, nameof(HomeIndexEventId));
+        public static EventId UsersIndexEventId = new EventId(200, nameof(UsersIndexEventId));
+    }
+}
