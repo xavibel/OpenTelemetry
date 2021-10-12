5c64fd5c99f057d2f6177e122c088490e182df30
Xavi Belloso <xbelloso@voxelgroup.net>
2021-10-12 23:54:38 +0200

    refact: remove unused events

--- WebPage/Diagnostics/WebPageClientIds.cs
+++ WebPage/Diagnostics/WebPageClientIds.cs
@@ -4,7 +4,6 @@ namespace WebPage.Diagnostics
 {
     public class WebPageClientIds
     {
-        public static EventId HomeIndexEventId = new EventId(100, nameof(HomeIndexEventId));
-        public static EventId UsersIndexEventId = new EventId(200, nameof(UsersIndexEventId));
+        public static EventId UsersIndexEventId = new(100, nameof(UsersIndexEventId));
     }
 }
